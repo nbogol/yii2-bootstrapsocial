9c9f55e84a22638c5b3acf07f441b24afaec48f3
Harry Tang <giaduy@gmail.com>
2014-10-27 20:27:31 +0700

    improvement

--- Widget.php
+++ Widget.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author: Harry Tang (giaduy@gmail.com)
+ * @link: http://www.greyneuron.com
+ * @copyright: Grey Neuron
+ */
 
 
 namespace harrytang\bootstrapsocial;
@@ -14,14 +19,13 @@ class Widget extends \yii\base\Widget
         $this->register();
     }
 
+    /**
+     * register asset
+     */
     protected function register()
     {
         $view = $this->getView();
-
         FontawesomeAsset::register($view);
         BootstrapsocialAsset::register($view);
-
-
-
     }
 }
